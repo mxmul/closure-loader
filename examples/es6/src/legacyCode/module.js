fd2c865e167a9e515310bdefd36e2f9ee1fe6ab5
Steven Weingärtner <sweingaertner@brainbits.net>
2016-07-19 18:08:45 +0200

    Add example for when require overwrites provide

--- examples/es6/src/legacyCode/module.js
+++ examples/es6/src/legacyCode/module.js
@@ -1,8 +1,11 @@
 goog.provide('some.legacy.namespace.myFn');
 goog.provide('some.legacy.namespace.someVar');
+goog.provide('some.namespace.submodule');
 
 goog.require('some.legacy.namespace.otherModule.text');
+goog.require('some.namespace');
 
+console.log('This should contain the empty submodule:', some.namespace);
 
 some.legacy.namespace.myFn = function () {
     console.log('this is some.legacy.namespace.myFn');
--- examples/es6/src/legacyCode/otherModule.js
+++ examples/es6/src/legacyCode/otherModule.js
@@ -1,4 +1,8 @@
 goog.provide('some.legacy.namespace.otherModule.text');
+goog.provide('some.namespace');
 
 
 some.legacy.namespace.otherModule.text = 'This is from otherModule.';
+some.namespace = {
+    value: 'This is a module',
+};
